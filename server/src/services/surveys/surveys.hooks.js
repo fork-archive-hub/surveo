d7ad0b95dcabb487e9958057c4c524701b31ab31
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-04-27 19:10:03 +0200

    Add hook calls in `survey` service
    - During creating survey, hooks are called to sets the author of survey
      based on the user logged in and remove the `votes` field from incoming
      survey data.
    - During removing surveys, hooks are called to restrict the deletion of
      surveys that are not owned by the user logged in.
    - In all responses, hooks are called to remove the `ips` field from
      survey data to protect user privacy.
    - During fetching list of available surveys, hooks are called to remove
      not necessary data from the response.
    - During fetching one specific survey, hooks are called to remove
      survey results if user is not the owner of the survey.

--- server/src/services/surveys/surveys.hooks.js
+++ server/src/services/surveys/surveys.hooks.js
@@ -1,20 +1,27 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const { iff, isNot, isProvider, discard, disallow } = require('feathers-hooks-common');
+
+const setDocumentOwner = require('../../hooks/set-document-owner');
+const deepDiscard = require('../../hooks/deep-discard');
+const restrictToOwner = require('../../hooks/restrict-to-owner');
+const isOwner = require('../../hooks/is-owner');
+
 module.exports = {
   before: {
     all: [authenticate('jwt')],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
+    create: [setDocumentOwner('author'), deepDiscard(['votes'])],
+    update: [disallow('external')],
+    patch: [disallow('external')],
+    remove: [restrictToOwner('author')],
   },
 
   after: {
-    all: [],
-    find: [],
-    get: [],
+    all: [iff(isProvider('external'), discard('ips'))],
+    find: [iff(isProvider('external'), deepDiscard(['votes']), discard('protection', 'questions'))],
+    get: [iff(isNot(isOwner('author')), deepDiscard(['votes']))],
     create: [],
     update: [],
     patch: [],
