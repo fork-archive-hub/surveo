f5204b379b76ee9aff3f4f6018a37c94d6320d05
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-05-19 17:30:12 +0200

    Remove timestamps fields from nested schemas

--- server/src/models/surveys.model.js
+++ server/src/models/surveys.model.js
@@ -2,54 +2,45 @@ module.exports = function (app) {
   const modelName = 'surveys';
   const mongooseClient = app.get('mongooseClient');
 
-  const AnswerSchema = new mongooseClient.Schema(
-    {
-      index: {
-        type: Number,
-        required: [true, 'Answer index is required'],
-      },
-      text: {
-        type: String,
-        required: [true, 'Answer text is required!'],
-        minlength: [2, 'Answer text must be at least 2 characters long!'],
-        maxLength: [140, 'Answer text cannot be more than 140 characters long!'],
-      },
-      votes: {
-        type: Number,
-        default: 0,
-        min: [0, 'Vote count cannot be less than 0!'],
-      },
+  const AnswerSchema = new mongooseClient.Schema({
+    index: {
+      type: Number,
+      required: [true, 'Answer index is required'],
     },
-    { timestamps: true }
-  );
+    text: {
+      type: String,
+      required: [true, 'Answer text is required!'],
+      minlength: [2, 'Answer text must be at least 2 characters long!'],
+      maxLength: [140, 'Answer text cannot be more than 140 characters long!'],
+    },
+    votes: {
+      type: Number,
+      default: 0,
+      min: [0, 'Vote count cannot be less than 0!'],
+    },
+  });
 
-  const SubQuestionSchema = new mongooseClient.Schema(
-    {
-      requirements: [Number],
-      text: {
-        type: String,
-        required: [true, 'Question text is required!'],
-        minlength: [3, 'Question text must be at least 3 characters long!'],
-        maxLength: [140, 'Question text cannot be more than 140 characters long!'],
-      },
-      answers: [AnswerSchema],
+  const SubQuestionSchema = new mongooseClient.Schema({
+    requirements: [Number],
+    text: {
+      type: String,
+      required: [true, 'Question text is required!'],
+      minlength: [3, 'Question text must be at least 3 characters long!'],
+      maxLength: [140, 'Question text cannot be more than 140 characters long!'],
     },
-    { timestamps: true }
-  );
+    answers: [AnswerSchema],
+  });
 
-  const QuestionSchema = new mongooseClient.Schema(
-    {
-      text: {
-        type: String,
-        required: [true, 'Question text is required!'],
-        minlength: [3, 'Question text must be at least 3 characters long!'],
-        maxLength: [140, 'Question text cannot be more than 140 characters long!'],
-      },
-      answers: [AnswerSchema],
-      subQuestions: [SubQuestionSchema],
+  const QuestionSchema = new mongooseClient.Schema({
+    text: {
+      type: String,
+      required: [true, 'Question text is required!'],
+      minlength: [3, 'Question text must be at least 3 characters long!'],
+      maxLength: [140, 'Question text cannot be more than 140 characters long!'],
     },
-    { timestamps: true }
-  );
+    answers: [AnswerSchema],
+    subQuestions: [SubQuestionSchema],
+  });
 
   const SurveySchema = new mongooseClient.Schema(
     {
