711c320c0217590b13ecdabdca317cf664a8d859
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-05-24 22:17:55 +0200

    Change privacy of `users` service methods
    - Revert `authenticate` hooks on `find` and `get` methods in `users`
      service.
    - Add updated `restrictToOwner` hook to `find`, `get`, `update`,
      `patch` and `remove` to prevent access to other users data.
    - Due to the updates to `users` service, change provider to `undefined`
      in `populate` hook inside `surveys` hooks.
    - Add `deepDiscard` hook to remove password from populated fields.

--- server/src/services/users/users.hooks.js
+++ server/src/services/users/users.hooks.js
@@ -4,18 +4,19 @@ const { hashPassword, protect } = require('@feathersjs/authentication-local').ho
 const { disallow } = require('feathers-hooks-common');
 
 const validateSchema = require('../../hooks/validate-schema.hook');
+const restrictToOwner = require('../../hooks/restrict-to-owner.hook');
 
 const { userSchema } = require('./users.schemas');
 
 module.exports = {
   before: {
     all: [],
-    find: [],
-    get: [],
+    find: [authenticate('jwt'), restrictToOwner('_id', '_id')],
+    get: [authenticate('jwt'), restrictToOwner('_id', '_id')],
     create: [validateSchema(userSchema), hashPassword('password')],
-    update: [disallow('external'), hashPassword('password'), authenticate('jwt')],
-    patch: [disallow('external'), hashPassword('password'), authenticate('jwt')],
-    remove: [disallow('external'), authenticate('jwt')],
+    update: [disallow('external'), hashPassword('password'), authenticate('jwt'), restrictToOwner('_id', '_id')],
+    patch: [disallow('external'), hashPassword('password'), authenticate('jwt'), restrictToOwner('_id', '_id')],
+    remove: [disallow('external'), authenticate('jwt'), restrictToOwner('_id', '_id')],
   },
 
   after: {
