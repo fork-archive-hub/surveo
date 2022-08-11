1bc0551863c7322a1296a0106457b830cac8efe2
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-08-11 02:52:28 +0200

    Update links in `QuestMenu` component
    - Replace `Button` component with `Link` component because Link fits
      the purpose better.
    - Add valid urls to `Link` component.

--- client/src/components/Layout/QuestMenu.jsx
+++ client/src/components/Layout/QuestMenu.jsx
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom';
-import { Stack, Tooltip, Button, Typography } from '@mui/material';
+import { Stack, Tooltip, Link as MUILink, Typography } from '@mui/material';
 
 const QuestMenu = () => {
   return (
-    <Stack direction="row" justifyContent="space-between" alignItems="center">
+    <Stack direction="row" justifyContent="space-between" alignItems="center" gap={1}>
       <Tooltip title="Login to your account">
-        <Button to="/" variant="text" size="small" component={Link}>
+        <MUILink to="/login" variant="body2" size="small" component={Link}>
           Sign in
-        </Button>
+        </MUILink>
       </Tooltip>
       <Typography variant="body2" color="textSecondary" align="center" sx={{ mx: 0.2 }}>
         or
       </Typography>
       <Tooltip title="Create a new account">
-        <Button to="/" variant="text" size="small" component={Link}>
+        <MUILink to="/register" variant="body2" size="small" component={Link}>
           Sign up
-        </Button>
+        </MUILink>
       </Tooltip>
     </Stack>
   );
