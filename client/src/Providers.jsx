a329e3ed0172d17a55ad3cf0dd30b20fdb17a4ac
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-08-11 19:11:58 +0200

    Create `react-toastify` to create a notifications
    - Install `react-toastify` package.
    - Add `react-toastify` provider to Provider component.

--- client/src/Providers.jsx
+++ client/src/Providers.jsx
@@ -8,12 +8,16 @@ import { theme } from './theme';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Providers = ({ children }) => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <BrowserRouter>{children}</BrowserRouter>
+        <ToastContainer theme="dark" position={toast.POSITION.TOP_CENTER} />
       </ThemeProvider>
     </Provider>
   );
