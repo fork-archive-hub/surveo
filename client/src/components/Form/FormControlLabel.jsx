5ee5e20204405a89346ee2f34cad75d716c4b58c
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-08-21 01:34:08 +0200

    Reset FormControlLabel horizontal margins
    - Reset FormControlLabel horizontal margins due to weird label
      positioning inside FormGroup and Stack components.

--- client/src/components/Form/FormControlLabel.jsx
+++ client/src/components/Form/FormControlLabel.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 import { FormControlLabel as MUIFormControlLabel } from '@mui/material';
 
 const FormControlLabel = forwardRef((props, ref) => {
-  return <MUIFormControlLabel size="small" ref={ref} {...props} />;
+  return <MUIFormControlLabel size="small" ref={ref} sx={{ mx: 0 }} {...props} />;
 });
 
 FormControlLabel.displayName = 'FormControlLabel';
