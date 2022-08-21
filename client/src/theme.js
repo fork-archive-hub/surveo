02dca044ada47960b45ad68ba5927ad1c254a733
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-08-21 18:55:43 +0200

    Add function to make fontSizes responsive

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,13 +1,15 @@
-import { createTheme } from '@mui/material';
+import { createTheme, responsiveFontSizes } from '@mui/material';
 
-export const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#d81b60',
+export const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#d81b60',
+      },
+      secondary: {
+        main: '#7b1fa2',
+      },
     },
-    secondary: {
-      main: '#7b1fa2',
-    },
-  },
-});
+  })
+);
