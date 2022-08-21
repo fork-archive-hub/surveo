96ddd18bb7fa3a1a678886079374d9765dc57479
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-08-21 12:35:31 +0200

    Create SurveyEditor component group
    - Create simple form that allows user to edit basic survey properties
      such as name, accessibility and protection.
    - `SurveyEditorForm` uses `ConfigurationFieldset` component from
      `SurveyBuilder` group.

---
+++ client/src/features/surveys/components/SurveyEditor/SurveyEditorForm.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+
+import { Box, Stack } from '@mui/material';
+
+import { useForm, FormProvider } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+
+import ConfigurationFieldset from '../SurveyBuilder/ConfigurationFieldset';
+
+import { surveyInformationFormSchema } from '../../schemas';
+
+import { SubmitButton } from '../../../../components/Form';
+
+const SurveyEditorForm = ({ survey, onUpdateSurvey }) => {
+  const methods = useForm({
+    mode: 'all',
+    defaultValues: {
+      name: survey.name,
+      open: survey.open,
+      protection: survey.protection,
+    },
+    resolver: joiResolver(surveyInformationFormSchema),
+  });
+
+  const onSubmit = async (data) => {
+    if (onUpdateSurvey) {
+      onUpdateSurvey(data);
+    }
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <Box sx={{ width: 360 }}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <Stack direction="column" spacing={2}>
+            <ConfigurationFieldset />
+            <SubmitButton>Save</SubmitButton>
+          </Stack>
+        </form>
+      </Box>
+    </FormProvider>
+  );
+};
+
+SurveyEditorForm.propTypes = {
+  survey: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    open: PropTypes.bool.isRequired,
+    protection: PropTypes.shape({
+      captcha: PropTypes.bool.isRequired,
+      ipRestriction: PropTypes.bool.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onUpdateSurvey: PropTypes.func.isRequired,
+};
+
+export default SurveyEditorForm;
--- client/src/features/surveys/index.js
+++ client/src/features/surveys/index.js
@@ -1,3 +1,4 @@
 export { default as SurveyBuilderForm } from './components/SurveyBuilder/SurveyBuilderForm';
+export { default as SurveyEditorForm } from './components/SurveyEditor/SurveyEditorForm';
 export { default as SurveyResults } from './components/SurveyResults/SurveyResults';
 export { default as SurveyForm } from './components/Survey/SurveyForm';
