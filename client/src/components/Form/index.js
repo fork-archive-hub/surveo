f43a0f02ac48d0f8a4772d350eafca878a3a5b07
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-08-26 11:52:51 +0200

    Create `IconButton` component

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as SubmitButton } from './SubmitButton';
+export { default as IconButton } from './IconButton';
 export { default as TextField } from './TextField';
 export { default as Radio } from './Radio';
 export { default as Checkbox } from './Checkbox';
