712f14b1e09f6386c5ea7d7d01eb1c20fe37673d
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-11-25 19:48:07 +0100

    Remove checking if method is callable
    - Remove checking if `validateAsync` in provided schema.
      It's not needed, because `validateAsync` is always present in schema
      created by `joi`.

--- server/src/hooks/validate-schema.hook.js
+++ server/src/hooks/validate-schema.hook.js
@@ -2,10 +2,6 @@ const { checkContext } = require('feathers-hooks-common');
 const { BadRequest } = require('@feathersjs/errors');
 
 module.exports = (schema) => {
-  if (!schema.validateAsync) {
-    throw new Error('Schema must have a `validateAsync` method');
-  }
-
   return async (context) => {
     checkContext(context, 'before', null, 'validateSchema');
 
