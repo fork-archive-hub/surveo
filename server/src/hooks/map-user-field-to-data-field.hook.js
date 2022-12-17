cc8e0d7a171b682e166faffc1c9dc36938455424
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-12-17 02:46:44 +0100

    Remove condition that checks if user is logged in
    - Remove condition that checks if user is logged in. This is not needed
      because the hooks are only called if the user is logged in.

--- server/src/hooks/map-user-field-to-data-field.hook.js
+++ server/src/hooks/map-user-field-to-data-field.hook.js
@@ -1,16 +1,9 @@
 const { checkContext } = require('feathers-hooks-common');
 const { setField } = require('feathers-authentication-hooks');
-const { Forbidden } = require('@feathersjs/errors');
 
 module.exports = (userField, dataField) => {
   return async (context) => {
     checkContext(context, 'before', null, 'mapUserFieldToDataField');
-
-    const isUserAuthenticated = Object.prototype.hasOwnProperty.call(context.params, 'user');
-
-    if (!isUserAuthenticated) {
-      throw new Forbidden('You must be authenticated to perform this action');
-    }
 
     await setField({ from: `params.user.${userField}`, as: `data.${dataField}` })(context);
   };
--- server/src/hooks/map-user-field-to-query-field.hook.js
+++ server/src/hooks/map-user-field-to-query-field.hook.js
@@ -1,6 +1,5 @@
 const { isProvider, checkContext } = require('feathers-hooks-common');
 const { setField } = require('feathers-authentication-hooks');
-const { Forbidden } = require('@feathersjs/errors');
 
 module.exports = (userField, queryField) => {
   return async (context) => {
@@ -8,12 +7,6 @@ module.exports = (userField, queryField) => {
 
     if (isProvider('server')(context)) {
       return;
-    }
-
-    const isUserAuthenticated = Object.prototype.hasOwnProperty.call(context.params, 'user');
-
-    if (!isUserAuthenticated) {
-      throw new Forbidden('You must be authenticated to perform this action');
     }
 
     await setField({ from: `params.user.${userField}`, as: `params.query.${queryField}` })(context);
