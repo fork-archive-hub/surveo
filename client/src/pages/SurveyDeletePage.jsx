0b91f6b79bcd546c08d3cce42109212ce30cb17f
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-01-05 07:25:15 +0100

    Add safe checks to `SurveyDeletePage`
    - Add function to check if current user is the owner of the survey
      that is being deleted.
    - Add function to check if any errors have occurred during the
      fetching of the survey.

--- client/src/pages/SurveyDeletePage.jsx
+++ client/src/pages/SurveyDeletePage.jsx
@@ -1,4 +1,6 @@
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { Grid } from '@mui/material';
@@ -6,15 +8,21 @@ import { Grid } from '@mui/material';
 import { toast } from 'react-toastify';
 
 import { useDocumentTitle } from '../hooks/useDocumentTitle';
+import { useGetSurveyQuery } from '../hooks/useGetSurveyQuery';
 
+import Spinner from '../components/elements/Spinner';
 import { SurveyDeleteDialog } from '../features/survey';
 
 import { feathers } from '../redux';
 
 const SurveyDeletePage = () => {
+  const user = useSelector((state) => state.authentication.user);
+
   const dispatch = useDispatch();
   const params = useParams();
   const navigate = useNavigate();
+
+  const { survey, isLoading, isError } = useGetSurveyQuery(params.surveyId);
 
   const handleCancel = () => {
     navigate('/');
@@ -37,12 +45,36 @@ const SurveyDeletePage = () => {
     navigate('/');
   };
 
+  useEffect(() => {
+    if (!isLoading && isError) {
+      navigate('/');
+    }
+  }, [isLoading, isError, navigate]);
+
+  useEffect(() => {
+    const validateSurveyAuthor = () => {
+      if (!survey._id) {
+        return;
+      }
+
+      if (survey._id === params.surveyId && survey.authorId !== user._id) {
+        toast.error('You are not authorized to delete this survey');
+        navigate('/');
+      }
+    };
+
+    validateSurveyAuthor();
+  }, [survey._id, params.surveyId, survey.authorId, user._id, navigate]);
+
   useDocumentTitle('Delete survey');
 
   return (
     <Grid container sx={{ justifyContent: 'center', py: 2 }}>
       <Grid item xs={12} sm={8} md={5} lg={4} xl={3}>
-        <SurveyDeleteDialog onDeleteSurvey={handleDelete} onCancelAction={handleCancel} />
+        {isLoading && <Spinner />}
+        {Boolean(survey._id) && !isLoading && !isError && (
+          <SurveyDeleteDialog onDeleteSurvey={handleDelete} onCancelAction={handleCancel} />
+        )}
       </Grid>
     </Grid>
   );
