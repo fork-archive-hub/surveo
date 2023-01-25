9ffc4888a1b68ef58b5d9dc5e5b968140eaa32a9
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-01-25 21:01:46 +0100

    Change styles of selected answers
    - Use `useRadioGroup` hook to detect if current answer is selected,
      based on the value of the radio group and the answer id.
    - Add additional vertical padding for radio button label stack.
    - Add right margin to images inside radio button label.
    - Add different color, background and border for selected answers
      based on `isChecked` value.

--- client/src/features/survey/components/sheet/Answer.jsx
+++ client/src/features/survey/components/sheet/Answer.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, useRadioGroup, alpha } from '@mui/material';
 
 import { findImageMarkdown } from '../../utils/findImageMarkdown';
 
@@ -9,18 +9,27 @@ import Radio from '../../../../components/form/Radio';
 import Image from '../../../../components/elements/Image';
 
 const Answer = ({ answer }) => {
+  const radioGroup = useRadioGroup();
+
+  const isChecked = radioGroup.value === answer._id;
   const image = findImageMarkdown(answer.text);
 
   return (
     <FormControlLabel
       key={answer._id}
       label={
-        <Stack spacing={0}>
+        <Stack spacing={0} sx={{ py: 1, '& > img': { mr: 1 } }}>
           <Typography variant="body1">{image.found ? image.title : answer.text}</Typography>
           {image.found && <Image url={image.url} title={image.title} />}
         </Stack>
       }
       control={<Radio size="small" value={answer._id} required />}
+      sx={{
+        borderRadius: 1,
+        color: (theme) => (isChecked ? theme.palette.primary.main : theme.palette.text.primary),
+        background: (theme) => (isChecked ? alpha(theme.palette.primary.main, 0.05) : 'transparent'),
+        border: (theme) => (isChecked ? `1px solid ${theme.palette.primary.main}` : '1px solid transparent'),
+      }}
     />
   );
 };
