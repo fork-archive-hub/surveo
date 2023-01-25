097b0b439d88480e344592115f9bf4f542fa9f93
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-01-25 18:47:25 +0100

    Add `forwardRef` to `Spinner` component
    - Add `forwardRef` to `Spinner` component to be consisten with other
      common components.

--- client/src/components/elements/Spinner.jsx
+++ client/src/components/elements/Spinner.jsx
@@ -1,11 +1,15 @@
+import { forwardRef } from 'react';
+
 import { Box, CircularProgress } from '@mui/material';
 
-const Spinner = () => {
+const Spinner = forwardRef((props, ref) => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: 1, height: 1 }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: 1, height: 1 }} ref={ref}>
       <CircularProgress />
     </Box>
   );
-};
+});
+
+Spinner.displayName = 'Spinner';
 
 export default Spinner;
